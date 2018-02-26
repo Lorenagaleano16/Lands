ce75db66682e5f56294e2d59c85843bb85e08c86
Lorenagaleano16 <36346224+Lorenagaleano16@users.noreply.github.com>
2018-02-26 10:10:30 -0500

    LOGIN

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace Lands
+{
+   
+    using Views;
+    using Xamarin.Forms;
+    public partial class App : Application
+	{
+        #region  Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+        #endregion
+    }
+}
